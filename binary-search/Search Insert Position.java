20f4d221f6d5498718b672f94390a38b3950c254
dty0102 <td324@cornell.edu>
2015-07-29 10:21:20 +0800

    Update Search Insert Position.java

--- binary-search/Search Insert Position.java
+++ binary-search/Search Insert Position.java
@@ -11,3 +11,23 @@ public int searchInsert(int[] A, int target) {
             return i;
         }
     }
+    
+Solution 2 :
+use binary search, O(log n)
+public int searchInsert(int[] A, int target) {
+        if(A==null||A.length==0) return 0;
+        int start=0; 
+        int size=A.length;
+        int end=size-1;
+        int mid;
+        if(A[0]>target) return 0;
+        if(A[size-1]<target) return size;
+        while(start+1<end){
+            mid=start+(end-start)/2;
+            if(A[mid]<target) start=mid;
+            else end=mid;
+        }
+        if(A[start]>=target) return start;
+        if(A[end]>=target) return end;
+        return 0;
+    }
