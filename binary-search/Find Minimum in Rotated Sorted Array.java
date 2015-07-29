83fe3c2b07f95f3f55232d9ee6b1a0687201abd2
dty0102 <td324@cornell.edu>
2015-07-29 10:22:48 +0800

    Create Find Minimum in Rotated Sorted Array.java

---
+++ binary-search/Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,25 @@
+Solution 1: O(n)
+public int findMin(int[] num) {
+        int min=num[0];
+        int n=num.length;
+        for(int i=1;i<n;i++){
+            if(num[i]<min){min=num[i];}
+        }
+        return min;
+    }
+    
+Solution 2 : O(log n)
+public int findMin(int[] num) {
+        if(num==null) return 0;
+        int start=0;
+        int end=num.length-1;
+        int mid;
+        while(start+1<end){
+            mid=start+(end-start)/2;
+            if(num[mid]>num[start]&&num[mid]<num[end]) return num[start];
+            if(num[mid]>num[start]) start=mid;
+            if(num[mid]<num[start]) end=mid;
+        }
+        if(num[start]>num[end]) return num[end];
+        else return num[start];
+    }
