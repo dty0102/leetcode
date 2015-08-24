0ba7dd0df78590f19be57ab5183541651df09d02
dty0102 <td324@cornell.edu>
2015-08-25 06:54:04 +0800

    Create Length of Last Word.java

---
+++ Length of Last Word.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] strings = s.split(" ");
+        if (strings.length == 0) return 0;
+        return strings[strings.length - 1].length();
+    }
+}
