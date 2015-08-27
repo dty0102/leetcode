71a581f594eee2efd184030ede873969d80beaf1
dty0102 <td324@cornell.edu>
2015-08-28 02:30:49 +0800

    Update valid Palindrome.java

--- valid Palindrome.java
+++ valid Palindrome.java
@@ -1,4 +1,4 @@
-Solution: pay attention to the situation that the string contains " ", and the lower and upper case. Use replaceAll to remove all non alphanumeric characters
+Solution 1 : pay attention to the situation that the string contains " ", and the lower and upper case. Use replaceAll to remove all non alphanumeric characters
 public class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^A-Za-z0-9]", "");
@@ -15,3 +15,16 @@ public class Solution {
         return true;
     }
 }
+Solution 2 : do not use the toLowercase function
+    public boolean isPalindrome(String s) {
+        // Write your code here
+        if (s == null || s.length() <= 1) {return true;}
+        s = s.replaceAll("[^a-zA-Z0-9]",""); ///!!!!!可以用replaceAll来将不符合的char消去
+        int start = 0;
+        int end = s.length() - 1;
+        while(start < end) {
+            if (s.charAt(start) == s.charAt(end) || s.charAt(start) == s.charAt(end) + 32 || s.charAt(start) == s.charAt(end) - 32) {start++; end--;}
+            else return false;
+        }
+        return true;
+    }
