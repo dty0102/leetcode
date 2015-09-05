05bc7baa945ec15b622d696a73d57d3c76f09949
dty0102 <td324@cornell.edu>
2015-09-04 21:36:35 -0400

    Create Best Time to Buy and Sell Stock.java

---
+++ Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        // write your code here
+        if(prices == null || prices.length == 0) {return 0;}
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i ++){
+            if (prices[i] < min) {min = prices[i];}
+            else {profit = Math.max(profit,prices[i] - min);} 
+        }
+        return profit;
+    }
+}
