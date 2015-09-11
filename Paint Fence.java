4a3b034cc4595d30c9e6b94cb8e93a6f26033d5a
dty0102 <td324@cornell.edu>
2015-09-10 23:29:56 -0400

    Update Paint Fence.java

--- Paint Fence.java
+++ Paint Fence.java
@@ -1,4 +1,7 @@
-Solution :
+Solution :We divided it into two cases.
+the last two posts have the same color, the number of ways to paint in this case is sameColorCounts.
+the last two posts have different colors, and the number of ways in this case is diffColorCounts.
+The reason why we have these two cases is that we can easily compute both of them, and that is all I do. When adding a new post, we can use the same color as the last one (if allowed) or different color. If we use different color, there're k-1 options, and the outcomes shoule belong to the diffColorCounts category. If we use same color, there's only one option, and we can only do this when the last two have different colors (which is the diffColorCounts). There we have our induction step.
 public int numWays(int n, int k) {
     if(n == 0) return 0;
     else if(n == 1) return k;
