eeae64dc425455594def0072177431d5017c29d2
dty0102 <td324@cornell.edu>
2015-09-11 13:11:37 -0400

    Create Hash Function.java

---
+++ Hash Function.java
@@ -0,0 +1,10 @@
+     public int hashCode(char[] key,int HASH_SIZE) {
+        // write your code here
+        
+        long res = 0;
+        for (int i = 0; i < key.length; i++){
+            res = 33 * res + (int)key[i];
+            res = res % HASH_SIZE;
+        }
+        return (int)res;
+     }
